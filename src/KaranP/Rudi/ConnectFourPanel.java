d899c60dac32044548d8906363447be0bd73d048
hrutvik patel <hrutvik@192.168.2.15>
2016-03-22 21:53:44 -0400

    update

--- src/KaranP/Rudi/ConnectFourPanel.java
+++ src/KaranP/Rudi/ConnectFourPanel.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.*;
 
 public class ConnectFourPanel extends JPanel {
 	private final static int GRID_ROW = 7, GRID_COL = 7, HGAP = 10, VGAP = 10;
@@ -13,10 +14,11 @@ public class ConnectFourPanel extends JPanel {
 	private static JButton[] placement;
 	private static int[][] grid;
 	private final static int BOARD_ROW = 6, BOARD_COL = 7;
-	private final static int EMPTY = 0, P1 = 1, P2 = 2;
+	private final static int EMPTY = 0, P1 = 1, P2 = 2, COMPUTER = 99;
 
-	private boolean turnP1;
-	private boolean turnP2;
+	private String gameType;
+	private int currentPlayer;
+	MultiPlayerConnect4 multiPlayer;
 
 	public ConnectFourPanel() {
 		setPreferredSize(new Dimension(770, 780));
@@ -51,16 +53,37 @@ public class ConnectFourPanel extends JPanel {
 			}
 		}
 		loadImageFile();
+		
+		multiPlayer = new MultiPlayerConnect4(grid);
 	}
 
 	private class ButtonActionListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			int columnPlaced;
-
-			if (event.getSource() == placement[0]) {
-				
+			for (int i = 0; i < placement.length; i++) {
+				if (event.getSource() == placement[i]) {
+					columnPlaced = i;
+					multiPlayer.switchCurrentPlayer();
+					currentPlayer = multiPlayer.getCurrentPlayer();
+					if (gameType.equals("MultiPlayer")){
+						if (currentPlayer == 1) {
+							multiPlayer.placeChip(columnPlaced, P1);
+						} else if (currentPlayer == 2) {
+							multiPlayer.placeChip(columnPlaced, P2);
+						}
+					}else if (gameType.equals("ComputerPlayer")){
+						
+					}
+					updateArray();
+					if(checkWin()){
+						if (currentPlayer == 1 || currentPlayer == 2){
+							JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " wins!!!");
+						}else if (currentPlayer == 99){
+							
+						}
+					}
+				}
 			}
-
 		}
 	}
 
@@ -85,17 +108,46 @@ public class ConnectFourPanel extends JPanel {
 		}
 	}
 
-	private void bluePlaced(int posRow, int posCol) {
+	public int getRandomPlayer(){
+		return multiPlayer.getRandomPlayer();
+	}
+	
+	private void updateArray(){
+		for (int i = 0; i < BOARD_ROW; i++) {
+			for (int j = 0; j < BOARD_COL; j++) {
+				grid[i][j] = multiPlayer.getCell(i,j);
+			}
+		}
+	}
+	
+	private static boolean isColumnFilled(int col) {
+		if (grid[0][col] != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void bluePlaced(int posRow, int posCol) {
 		board[posRow][posCol].setBackground(Color.BLUE);
-
+		//switchPlayer = true;
+		if (isColumnFilled(posCol)) {
+			placement[posCol].setEnabled(false);
+			placement[posCol].setToolTipText("Column FILLED");
+		}
 	}
 
-	private void redPlaced(int posRow, int posCol) {
+	public static void redPlaced(int posRow, int posCol) {
 		board[posRow][posCol].setBackground(Color.RED);
+		//switchPlayer = true;
+		if (isColumnFilled(posCol)) {
+			placement[posCol].setEnabled(false);
+			placement[posCol].setToolTipText("Column FILLED");
+		}
 	}
 
-	public void setBoardVisible(boolean visible){
-		if(visible){
+	public void setBoardVisible(boolean visible) {
+		if (visible) {
 			for (int i = 0; i < placement.length; i++) {
 				placement[i].setEnabled(true);
 			}
@@ -104,7 +156,7 @@ public class ConnectFourPanel extends JPanel {
 					board[i][j].setEnabled(true);
 				}
 			}
-		}else if(!visible){
+		} else if (!visible) {
 			for (int i = 0; i < placement.length; i++) {
 				placement[i].setEnabled(false);
 			}
@@ -115,4 +167,85 @@ public class ConnectFourPanel extends JPanel {
 			}
 		}
 	}
+
+	private void hasWon(int c1x, int c1y, int c2x, int c2y, int c3x, int c3y, int c4x, int c4y) {
+		setBoardVisible(false);
+
+		board[c1x][c1y].setEnabled(true);
+		board[c2x][c2y].setEnabled(true);
+		board[c3x][c3y].setEnabled(true);
+		board[c4x][c4y].setEnabled(true);
+
+	}
+
+	private boolean checkWin() {
+		int tempCheck;
+		// vertical win
+		for (int i = BOARD_ROW - 1; i >= 2; i--) {
+			for (int j = BOARD_COL - 1; j >= 0; j--) {
+				tempCheck = grid[i][j];
+				if (tempCheck != EMPTY && tempCheck == grid[i - 1][j] && tempCheck == grid[i - 2][j]
+						&& tempCheck == grid[i - 3][j]) {
+					hasWon(i, j, i - 1, j, i - 2, j, i - 3, j);
+					return true;
+				}
+			}
+		}
+		// horizontal win
+		for (int i = BOARD_ROW - 1; i >= 0; i--) {
+			for (int j = 0; j < BOARD_COL - 3; j++) {
+				tempCheck = grid[i][j];
+				if (tempCheck != EMPTY && tempCheck == grid[i][j + 1] && tempCheck == grid[i][j + 2]
+						&& tempCheck == grid[i][j + 3]) {
+					hasWon(i, j, i, j + 1, i, j + 2, i, j + 3);
+					return true;
+				}
+			}
+		}
+		// Diagonal win 
+		for (int i = 0; i < BOARD_ROW - 3; i++) {
+			for (int j = 0; j < BOARD_COL - 3; j++) {
+				tempCheck = grid[i][j];
+				if (tempCheck != EMPTY && tempCheck == grid[i + 1][j + 1] && tempCheck == grid[i + 2][j + 2]
+						&& tempCheck == grid[i + 3][j + 3]) {
+					hasWon(i, j, i + 1, j + 1, i + 2, j + 2, i + 3, j + 3);
+					return true;
+				}
+			}
+		}
+		for (int i = BOARD_ROW - 3; i < BOARD_ROW; i++) {
+			for (int j = 0; j < BOARD_COL - 3; j++) {
+				tempCheck = grid[i][j];
+				if (tempCheck != EMPTY && tempCheck == grid[i - 1][j + 1] && tempCheck == grid[i - 2][j + 2]
+						&& tempCheck == grid[i - 3][j + 3]) {
+					hasWon(i, j, i - 1, j + 1, i - 2, j + 2, i - 3, j + 3);
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+
+	public void gameReset() {
+		for (int i = 0; i < BOARD_ROW; i++) {
+			for (int j = 0; j < BOARD_COL; j++) {
+				grid[i][j] = EMPTY;
+				board[i][j].setBackground(Color.WHITE);
+				board[i][j].setEnabled(false);
+			}
+		}
+		for (int i = 0; i < placement.length; i++) {
+			placement[i].setEnabled(false);
+			placement[i].setToolTipText("");
+		}
+		currentPlayer = EMPTY;
+		gameType = "";
+		//multiPlayer = new MultiPlayerConnect4(grid);
+	}
+
+	public void setGameMode(String type) {
+		this.gameType = type;
+	}
 }
+
--- src/KaranP/Rudi/SetUpPanel.java
+++ src/KaranP/Rudi/SetUpPanel.java
@@ -6,14 +6,13 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-
-public class SetUpPanel extends JPanel{
-	private static JButton twoPlayersBtn, playAIBtn;
+public class SetUpPanel extends JPanel {
+	private static JButton twoPlayersBtn, playAIBtn, newGameBtn, randomPlayerBtn;
 	private static JLabel player1Lbl, player2Lbl, AILbl;
 	private JPanel mainPanel, setUpPanel;
-	private ConnectFourPanel startConnectFour;
-	
-	public SetUpPanel(){
+	private ConnectFourBoard startConnectFour;
+
+	public SetUpPanel() {
 		// Buttons
 		twoPlayersBtn = new JButton("Play With Two Player");
 		twoPlayersBtn.setFont(new Font("Sans Serif", Font.ITALIC, 20));
@@ -21,14 +20,28 @@ public class SetUpPanel extends JPanel{
 		twoPlayersBtn.setContentAreaFilled(true);
 		twoPlayersBtn.setEnabled(true);
 		twoPlayersBtn.addActionListener(new ButtonActionListener());
-		
+
 		playAIBtn = new JButton("Play With Computer");
 		playAIBtn.setFont(new Font("Sans Serif", Font.ITALIC, 20));
 		playAIBtn.setHorizontalAlignment(SwingConstants.CENTER);
 		playAIBtn.setContentAreaFilled(true);
 		playAIBtn.setEnabled(false);
 		playAIBtn.addActionListener(new ButtonActionListener());
-		
+
+		newGameBtn = new JButton("Start A New Game");
+		newGameBtn.setFont(new Font("Sans Serif", Font.ITALIC, 20));
+		newGameBtn.setHorizontalAlignment(SwingConstants.CENTER);
+		newGameBtn.setContentAreaFilled(true);
+		newGameBtn.setEnabled(true);
+		newGameBtn.addActionListener(new ButtonActionListener());
+
+		randomPlayerBtn = new JButton("Choose a Random Player");
+		randomPlayerBtn.setFont(new Font("Sans Serif", Font.ITALIC, 20));
+		randomPlayerBtn.setHorizontalAlignment(SwingConstants.CENTER);
+		randomPlayerBtn.setContentAreaFilled(true);
+		randomPlayerBtn.setEnabled(true);
+		randomPlayerBtn.addActionListener(new ButtonActionListener());
+
 		// Labels
 		player1Lbl = new JLabel("Player 1 > Blue Chip");
 		player1Lbl.setFont(new Font("Sans Serif", Font.BOLD, 24));
@@ -37,7 +50,7 @@ public class SetUpPanel extends JPanel{
 		player1Lbl.setBackground(Color.BLUE);
 		player1Lbl.setForeground(Color.WHITE);
 		player1Lbl.setBorder(BorderFactory.createLineBorder(Color.WHITE));
-		
+
 		player2Lbl = new JLabel("Player 2 > Red Chip");
 		player2Lbl.setFont(new Font("Sans Serif", Font.BOLD, 24));
 		player2Lbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -45,7 +58,7 @@ public class SetUpPanel extends JPanel{
 		player2Lbl.setBackground(Color.RED);
 		player2Lbl.setForeground(Color.WHITE);
 		player2Lbl.setBorder(BorderFactory.createLineBorder(Color.WHITE));
-		
+
 		AILbl = new JLabel("Computer > Red Chip");
 		AILbl.setFont(new Font("Sans Serif", Font.BOLD, 24));
 		AILbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -53,51 +66,82 @@ public class SetUpPanel extends JPanel{
 		AILbl.setBackground(Color.RED);
 		AILbl.setForeground(Color.WHITE);
 		AILbl.setBorder(BorderFactory.createLineBorder(Color.WHITE));
-		
-		startConnectFour = new ConnectFourPanel();
-		
+
+		startConnectFour = new ConnectFourBoard();
+
 		// Set up the panels
 		setUpPanel = new JPanel();
 		setUpPanel.setBackground(Color.BLACK);
-		setUpPanel.setLayout(new GridLayout(1, 2, 40, 40));
+		// setUpPanel.setLayout(new GridLayout(1, 2, 40, 40));
+		setUpPanel.setLayout(new FlowLayout(5, 40, 2));
 		setUpPanel.add(twoPlayersBtn);
 		setUpPanel.add(playAIBtn);
-		
+
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
 		setBackground(Color.BLACK);
-		setPreferredSize(new Dimension(790,840));
+		setPreferredSize(new Dimension(790, 840));
 		add(setUpPanel, BorderLayout.NORTH);
 		add(startConnectFour, BorderLayout.CENTER);
 	}
-	
+
 	private class ButtonActionListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
-			if (event.getSource() == twoPlayersBtn) {	
+			if (event.getSource() == twoPlayersBtn) {
+				startConnectFour.setGameMode("MultiPlayer");
 				twoPlayersBtn.setVisible(false);
 				playAIBtn.setVisible(false);
-				
+
 				setUpPanel.remove(twoPlayersBtn);
 				setUpPanel.remove(playAIBtn);
-				setUpPanel.add(player1Lbl);
-				setUpPanel.add(player2Lbl);
+				setUpPanel.add(randomPlayerBtn);
+				randomPlayerBtn.setVisible(true);				
 				
-				startConnectFour.setBoardVisible(true);
-			}
-			
-			if (event.getSource() == playAIBtn) {	
+			}if (event.getSource() == playAIBtn) {
+				startConnectFour.setGameMode("ComputerPlayer");
 				twoPlayersBtn.setVisible(false);
 				playAIBtn.setVisible(false);
-				
+
 				setUpPanel.remove(twoPlayersBtn);
 				setUpPanel.remove(playAIBtn);
 				setUpPanel.add(player1Lbl);
 				setUpPanel.add(AILbl);
 				startConnectFour.setBoardVisible(true);
 				
+			}if (event.getSource() == randomPlayerBtn) {
+				int currentPlayer = startConnectFour.getRandomPlayer();
+				JOptionPane.showMessageDialog(null, "Player " + currentPlayer +" goes first");
+				randomPlayerBtn.setVisible(false);
+				setUpPanel.remove(randomPlayerBtn);
+				
+				if(currentPlayer == 1){
+					player1Lbl.setText("***Player 1 > Blue Chip");
+				}else if(currentPlayer == 2){
+					player2Lbl.setText("***Player 2 > Red Chip");
+				}			
+				setUpPanel.add(newGameBtn);
+				setUpPanel.add(player1Lbl);
+				setUpPanel.add(player2Lbl);
+				
+				startConnectFour.setBoardVisible(true);
+				
+			}if (event.getSource() == newGameBtn) {
+				startConnectFour.gameReset();
+				startConnectFour.setBoardVisible(false);
+				
+				setUpPanel.remove(newGameBtn);
+				setUpPanel.remove(player1Lbl);
+				setUpPanel.remove(player2Lbl);				
+				
+				setUpPanel.add(twoPlayersBtn);
+				setUpPanel.add(playAIBtn);
+				twoPlayersBtn.setVisible(true);
+				playAIBtn.setVisible(true);
+				
+				player1Lbl.setText("Player 1 > Blue Chip");
+				player2Lbl.setText("Player 2 > Red Chip");
 			}
-
-
-		}
+			
+		}		
 	}
 }
