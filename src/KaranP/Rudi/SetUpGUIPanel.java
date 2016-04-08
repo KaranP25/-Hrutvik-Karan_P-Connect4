dbc2d41e9de2d515ef33e1eb1d9bec12eab4fd6b
KaranP25 <karan8patel@gmail.com>
2016-04-08 12:29:21 -0400

    Update SetUpGUIPanel.java

--- src/KaranP/Rudi/SetUpGUIPanel.java
+++ src/KaranP/Rudi/SetUpGUIPanel.java
@@ -14,63 +14,68 @@ import javax.swing.*;
  * @author hrutvik and karan
  *
  */
-public class SetUpGUIPanel extends JPanel {
+public class SetUpGUIPanel extends JPanel implements ActionListener {
+	private final int MAIN_PANEL_WIDTH = 450, MAIN_PANEL_HEIGHT = 480;
 	private static JButton twoPlayersBtn, playAIBtn, newGameBtn, randomPlayerBtn, playerGoFirstBtn, compGoFirstBtn;
 	private static JLabel player1Lbl, player2Lbl, AILbl, startScreen;
 	private JPanel mainPanel, setUpPanel;
 	private ConnectFourPanel startConnectFour;
-	
+
 	private boolean firstStart = true;
 
+	/**
+	 * This Constructor method sets up the GUI menu and connect 4 board
+	 */
+	@SuppressWarnings("static-access")
 	public SetUpGUIPanel() {
 		// Buttons
-		twoPlayersBtn = new JButton("Play With Two Player");
+		this.twoPlayersBtn = new JButton("Play With Two Player");
 		twoPlayersBtn.setFont(new Font("Sans Serif", Font.ITALIC, 14));
 		twoPlayersBtn.setHorizontalAlignment(SwingConstants.CENTER);
 		twoPlayersBtn.setContentAreaFilled(true);
 		twoPlayersBtn.setEnabled(true);
-		twoPlayersBtn.addActionListener(new ButtonActionListener());
+		twoPlayersBtn.addActionListener(this);
 
-		playAIBtn = new JButton("Play With Computer");
+		this.playAIBtn = new JButton("Play With Computer");
 		playAIBtn.setFont(new Font("Sans Serif", Font.ITALIC, 14));
 		playAIBtn.setHorizontalAlignment(SwingConstants.CENTER);
 		playAIBtn.setContentAreaFilled(true);
 		playAIBtn.setEnabled(true);
-		playAIBtn.addActionListener(new ButtonActionListener());
+		playAIBtn.addActionListener(this);
 
-		newGameBtn = new JButton("Start A New Game");
+		this.newGameBtn = new JButton("Start A New Game");
 		newGameBtn.setFont(new Font("Sans Serif", Font.ITALIC, 14));
 		newGameBtn.setHorizontalAlignment(SwingConstants.CENTER);
 		newGameBtn.setContentAreaFilled(true);
 		newGameBtn.setEnabled(true);
-		newGameBtn.addActionListener(new ButtonActionListener());
+		newGameBtn.addActionListener(this);
 
-		randomPlayerBtn = new JButton("Choose a Random Player");
+		this.randomPlayerBtn = new JButton("Choose a Random Player");
 		randomPlayerBtn.setFont(new Font("Sans Serif", Font.ITALIC, 14));
 		randomPlayerBtn.setHorizontalAlignment(SwingConstants.CENTER);
 		randomPlayerBtn.setContentAreaFilled(true);
 		randomPlayerBtn.setEnabled(true);
-		randomPlayerBtn.addActionListener(new ButtonActionListener());
+		randomPlayerBtn.addActionListener(this);
 
-		playerGoFirstBtn = new JButton("I will Go First");
+		this.playerGoFirstBtn = new JButton("I will Go First");
 		playerGoFirstBtn.setFont(new Font("Sans Serif", Font.ITALIC, 14));
 		playerGoFirstBtn.setHorizontalAlignment(SwingConstants.CENTER);
 		playerGoFirstBtn.setContentAreaFilled(true);
 		playerGoFirstBtn.setEnabled(true);
-		playerGoFirstBtn.addActionListener(new ButtonActionListener());
+		playerGoFirstBtn.addActionListener(this);
 
-		compGoFirstBtn = new JButton("Computer Goes First");
+		this.compGoFirstBtn = new JButton("Computer Goes First");
 		compGoFirstBtn.setFont(new Font("Sans Serif", Font.ITALIC, 14));
 		compGoFirstBtn.setHorizontalAlignment(SwingConstants.CENTER);
 		compGoFirstBtn.setContentAreaFilled(true);
 		compGoFirstBtn.setEnabled(true);
-		compGoFirstBtn.addActionListener(new ButtonActionListener());
+		compGoFirstBtn.addActionListener(this);
 
 		// Labels
 		startScreen = new JLabel();
 		startScreen.setHorizontalAlignment(SwingConstants.CENTER);
-		loadBackgroundImage();		
-		
+		loadBackgroundImage();
+
 		player1Lbl = new JLabel("Player 1 > Blue Chip");
 		player1Lbl.setFont(new Font("Sans Serif", Font.BOLD, 12));
 		player1Lbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -106,114 +111,117 @@ public class SetUpGUIPanel extends JPanel {
 
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
-		setPreferredSize(new Dimension(450, 480));
+		setPreferredSize(new Dimension(MAIN_PANEL_WIDTH, MAIN_PANEL_HEIGHT));
 		setBackground(Color.BLACK);
 		add(setUpPanel, BorderLayout.NORTH);
 		setBackgroundImage(true);
 	}
 
-	private class ButtonActionListener implements ActionListener {
-		public void actionPerformed(ActionEvent event) {
-			if (event.getSource() == twoPlayersBtn) {
-				if(firstStart){
-					setBackgroundImage(false);
-					firstStart = false;
-				}
-				
-				startConnectFour.setGameMode("MultiPlayerConnect4");
-				twoPlayersBtn.setVisible(false);
-				playAIBtn.setVisible(false);
-
-				setUpPanel.remove(twoPlayersBtn);
-				setUpPanel.remove(playAIBtn);
-				setUpPanel.add(randomPlayerBtn);
-				randomPlayerBtn.setVisible(true);
-
+	/**
+	 * This method is from the ActionListener class and handles events
+	 */
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource() == twoPlayersBtn) {
+			if (firstStart) {
+				setBackgroundImage(false);
+				firstStart = false;
 			}
-			if (event.getSource() == playAIBtn) {
-				if(firstStart){
-					setBackgroundImage(false);
-					firstStart = false;
-				}
-				
-				startConnectFour.setGameMode("ComputerPlayer");
-				twoPlayersBtn.setVisible(false);
-				playAIBtn.setVisible(false);
 
-				setUpPanel.remove(twoPlayersBtn);
-				setUpPanel.remove(playAIBtn);
+			startConnectFour.setGameMode("MultiPlayerConnect4");
+			twoPlayersBtn.setVisible(false);
+			playAIBtn.setVisible(false);
 
-				setUpPanel.add(playerGoFirstBtn);
-				setUpPanel.add(compGoFirstBtn);
-
-			}
-			if (event.getSource() == randomPlayerBtn) {
-				int currentPlayer = startConnectFour.getRandomPlayer();
-				JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " goes first");
-				randomPlayerBtn.setVisible(false);
-				setUpPanel.remove(randomPlayerBtn);
-
-				if (currentPlayer == 1) {
-					player1Lbl.setText("***Player 1 > Blue Chip");
-				} else if (currentPlayer == 2) {
-					player2Lbl.setText("***Player 2 > Red Chip");
-				}
-				setUpPanel.add(newGameBtn);
-				setUpPanel.add(player1Lbl);
-				setUpPanel.add(player2Lbl);
-
-				startConnectFour.setBoardVisible(true);
-
-			}
-			if (event.getSource() == newGameBtn) {
-				startConnectFour.gameReset();
-				startConnectFour.setBoardVisible(false);
-
-				setUpPanel.remove(newGameBtn);
-				setUpPanel.remove(player1Lbl);
-				setUpPanel.remove(player2Lbl);
-				setUpPanel.remove(AILbl);
-
-				setUpPanel.add(twoPlayersBtn);
-				setUpPanel.add(playAIBtn);
-				twoPlayersBtn.setVisible(true);
-				playAIBtn.setVisible(true);
-				playerGoFirstBtn.setVisible(true);
-				compGoFirstBtn.setVisible(true);
-
-				player1Lbl.setText("Player 1 > Blue Chip");
-				player2Lbl.setText("Player 2 > Red Chip");
-
-			}
-			if (event.getSource() == playerGoFirstBtn) {
-				playerGoFirstBtn.setVisible(false);
-				compGoFirstBtn.setVisible(false);
-				setUpPanel.remove(playerGoFirstBtn);
-				setUpPanel.remove(compGoFirstBtn);
-
-				setUpPanel.add(newGameBtn);
-				setUpPanel.add(player1Lbl);
-				setUpPanel.add(AILbl);
-				startConnectFour.setBoardVisible(true);
-				
-				startConnectFour.setTurn(1); // 1 represents the human
-			}
-			if (event.getSource() == compGoFirstBtn) {
-				playerGoFirstBtn.setVisible(false);
-				compGoFirstBtn.setVisible(false);
-				setUpPanel.remove(playerGoFirstBtn);
-				setUpPanel.remove(compGoFirstBtn);
-
-				setUpPanel.add(newGameBtn);
-				setUpPanel.add(player1Lbl);
-				setUpPanel.add(AILbl);
-				startConnectFour.setBoardVisible(true);
-				
-				startConnectFour.setTurn(99); // 99 represent the computer
-				startConnectFour.computersTurn();				
-			}
+			setUpPanel.remove(twoPlayersBtn);
+			setUpPanel.remove(playAIBtn);
+			setUpPanel.add(randomPlayerBtn);
+			randomPlayerBtn.setVisible(true);
 
 		}
+		if (event.getSource() == playAIBtn) {
+			if (firstStart) {
+				setBackgroundImage(false);
+				firstStart = false;
+			}
+
+			startConnectFour.setGameMode("ComputerPlayer");
+			twoPlayersBtn.setVisible(false);
+			playAIBtn.setVisible(false);
+
+			setUpPanel.remove(twoPlayersBtn);
+			setUpPanel.remove(playAIBtn);
+
+			setUpPanel.add(playerGoFirstBtn);
+			setUpPanel.add(compGoFirstBtn);
+
+		}
+		if (event.getSource() == randomPlayerBtn) {
+			int currentPlayer = startConnectFour.getRandomPlayer();
+			JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " goes first");
+			randomPlayerBtn.setVisible(false);
+			setUpPanel.remove(randomPlayerBtn);
+
+			if (currentPlayer == 1) {
+				player1Lbl.setText("***Player 1 > Blue Chip");
+			} else if (currentPlayer == 2) {
+				player2Lbl.setText("***Player 2 > Red Chip");
+			}
+			setUpPanel.add(newGameBtn);
+			setUpPanel.add(player1Lbl);
+			setUpPanel.add(player2Lbl);
+
+			startConnectFour.setBoardVisible(true);
+
+		}
+		if (event.getSource() == newGameBtn) {
+			startConnectFour.gameReset();
+			startConnectFour.setBoardVisible(false);
+
+			setUpPanel.remove(newGameBtn);
+			setUpPanel.remove(player1Lbl);
+			setUpPanel.remove(player2Lbl);
+			setUpPanel.remove(AILbl);
+
+			setUpPanel.add(twoPlayersBtn);
+			setUpPanel.add(playAIBtn);
+			twoPlayersBtn.setVisible(true);
+			playAIBtn.setVisible(true);
+			playerGoFirstBtn.setVisible(true);
+			compGoFirstBtn.setVisible(true);
+
+			player1Lbl.setText("Player 1 > Blue Chip");
+			player2Lbl.setText("Player 2 > Red Chip");
+			AILbl.setText("Computer > Blue Chip");
+		}
+		if (event.getSource() == playerGoFirstBtn) {
+			playerGoFirstBtn.setVisible(false);
+			compGoFirstBtn.setVisible(false);
+			setUpPanel.remove(playerGoFirstBtn);
+			setUpPanel.remove(compGoFirstBtn);
+
+			player1Lbl.setText("***Player > Blue Chip");
+			setUpPanel.add(newGameBtn);
+			setUpPanel.add(player1Lbl);
+			setUpPanel.add(AILbl);
+			startConnectFour.setBoardVisible(true);
+
+			startConnectFour.setTurn(1); // 1 represents the human
+		}
+		if (event.getSource() == compGoFirstBtn) {
+			playerGoFirstBtn.setVisible(false);
+			compGoFirstBtn.setVisible(false);
+			setUpPanel.remove(playerGoFirstBtn);
+			setUpPanel.remove(compGoFirstBtn);
+
+			AILbl.setText("***Computer > Blue Chip");
+			setUpPanel.add(newGameBtn);
+			setUpPanel.add(player1Lbl);
+			setUpPanel.add(AILbl);
+			startConnectFour.setBoardVisible(true);
+
+			startConnectFour.setTurn(99); // 99 represent the computer
+			startConnectFour.computersTurn();
+		}
+
 	}
 	
 	private void loadBackgroundImage(){
